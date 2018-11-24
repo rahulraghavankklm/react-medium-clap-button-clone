d43e8d8df9bbb27266df0629ebc46d1222238fd4
Rahul Raghavan <rahulraghavankklm@gmail.com>
2018-11-24 11:44:34 +0530

    handle posting of claps both single claps and repeated claps

--- src/components/ClapButton/ClapButton.js
+++ src/components/ClapButton/ClapButton.js
@@ -1,5 +1,7 @@
 import React from "react";
 import styled, { css, keyframes } from "styled-components";
+import mojs from "mo-js";
+import debounce from 'lodash/debounce'
 
 const shockwave = keyframes`
   0% {
@@ -73,7 +75,20 @@ const ClapCount = styled.div`
   padding: 4px;
   margin: 8px;
   display: flex;
-  transition-delay: 300ms;
+  transition: all 0.2s ease-in-out;
+
+  ${props =>
+    props.isClapping &&
+    css`
+      opacity: 0;
+      color: green;
+    `}
+
+  ${props =>
+    !props.isClapping &&
+    css`
+      opacity: 1;
+    `}
 `;
 
 const ClearClaps = styled.button`
@@ -105,20 +120,20 @@ const ClapsSessionCount = styled.div`
   font-size: 16px;
   padding: 4px;
   position: relative;
-  top: 0;
+  top: 40px;
   transition: all 0.2s ease-in-out;
 
   ${props =>
     props.isClapping &&
     css`
-      top: -40px;
+      top: -30px;
       opacity: 1;
     `}
 
   ${props =>
     !props.isClapping &&
     css`
-      top: 0px;
+      top: -80px;
       opacity: 0;
     `}
 `;
@@ -128,27 +143,27 @@ class ClapButtonContainer extends React.Component {
     super(props);
     this.state = {
       userClaps: 0,
-      totalClaps: 0,
+      totalClaps: 100,
       clapsInARow: 0,
       isClapped: false,
-      isClapping: false
+      isClapping: false,
     };
     this.clapButton = React.createRef();
   }
 
-  // handleClapping = e => {
-  //   e.preventDefault();
-  //   this.handleClap()
-  // };
-
-  handleClearClaps = e => {
+  handleClearClaps = async (e) => {
     e.preventDefault();
-    this.setState(prevState => ({
-      isClapped: false,
-      userClaps: 0,
-      totalClaps: prevState.totalClaps - prevState.userClaps,
-      clapsInARow: 0
-    }));
+    this.setState(
+      prevState => ({
+        isClapped: false,
+        userClaps: 0,
+        totalClaps: prevState.totalClaps - prevState.userClaps,
+        clapsInARow: 0
+      }),
+      () => {
+        // this.postClapsData()
+      }
+    );
   };
 
   handleClap = async () => {
@@ -163,34 +178,37 @@ class ClapButtonContainer extends React.Component {
       totalClaps: prevState.totalClaps + 1,
       isClapped: true,
       isClapping: true
-    }));
+    }), () => {
+      // this.postClapsData();
+    });
   };
 
-  handleSingleClap = e => {
+  handleSingleClap = async (e) => {
     e.preventDefault();
     this.handleClap();
   };
 
-  handleRepeatClapStart = e => {
+  handleRepeatClapStart = async (e) => {
     this.clapRepeatTimer = setInterval(() => {
       this.handleClap();
       this.addClapSession();
     }, 100);
   };
 
-  handleRepeatClapEnd = e => {
+  handleRepeatClapEnd = async (e) => {
     e.preventDefault();
-    this.removeClapsSession();
     clearInterval(this.clapRepeatTimer);
+    this.removeClapsSession();
+    // this.postClapsData()
   };
 
-  addClapSession = () => {
+  addClapSession = async () => {
     this.setState(prevState => ({
       clapsInARow: prevState.clapsInARow + 1
     }));
   };
 
-  removeClapsSession = () => {
+  removeClapsSession = async () => {
     setTimeout(() => {
       this.setState({
         clapsInARow: 0
@@ -199,6 +217,9 @@ class ClapButtonContainer extends React.Component {
   };
 
   componentDidMount() {
+
+    this.getClapsData()
+
     const clapBtn = this.clapButton.current;
     clapBtn.addEventListener("mousedown", this.handleRepeatClapStart);
     clapBtn.addEventListener("mouseup", this.handleRepeatClapEnd);
@@ -212,6 +233,20 @@ class ClapButtonContainer extends React.Component {
     clapBtn.removeEventListener("click", this.handleSingleClap);
   }
 
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.userClaps !== this.state.userClaps) {
+      this.postClapsData()
+    }
+  }
+
+  getClapsData = async () => {
+    console.log("GET claps from API: ", this.state.totalClaps);
+  }
+
+  postClapsData = debounce(async () => {
+    console.log("POST claps to API: ", this.state.userClaps);
+  }, 100);
+
   render() {
     const {
       userClaps,
@@ -221,7 +256,10 @@ class ClapButtonContainer extends React.Component {
       clapsInARow
     } = this.state;
 
-    console.log(this.state);
+    const iconStyle = {
+      fill: isClapped ? "green" : "transparent",
+      stroke: "green"
+    };
 
     return (
       <ClapButtonWrap>
@@ -230,7 +268,6 @@ class ClapButtonContainer extends React.Component {
           {clapsInARow}
         </ClapsSessionCount>
         <ClapButton
-          // onClick={this.handleClapping}
           isClapping={isClapping}
           clapped={isClapped}
           ref={this.clapButton}
@@ -240,7 +277,7 @@ class ClapButtonContainer extends React.Component {
               id="clap--icon"
               xmlns="http://www.w3.org/2000/svg"
               viewBox="-549 338 100.1 125"
-              style={{ fill: isClapped ? "green" : "gray" }}
+              style={iconStyle}
             >
               <path d="M-471.2 366.8c1.2 1.1 1.9 2.6 2.3 4.1.4-.3.8-.5 1.2-.7 1-1.9.7-4.3-1-5.9-2-1.9-5.2-1.9-7.2.1l-.2.2c1.8.1 3.6.9 4.9 2.2zm-28.8 14c.4.9.7 1.9.8 3.1l16.5-16.9c.6-.6 1.4-1.1 2.1-1.5 1-1.9.7-4.4-.9-6-2-1.9-5.2-1.9-7.2.1l-15.5 15.9c2.3 2.2 3.1 3 4.2 5.3zm-38.9 39.7c-.1-8.9 3.2-17.2 9.4-23.6l18.6-19c.7-2 .5-4.1-.1-5.3-.8-1.8-1.3-2.3-3.6-4.5l-20.9 21.4c-10.6 10.8-11.2 27.6-2.3 39.3-.6-2.6-1-5.4-1.1-8.3z" />
               <path d="M-527.2 399.1l20.9-21.4c2.2 2.2 2.7 2.6 3.5 4.5.8 1.8 1 5.4-1.6 8l-11.8 12.2c-.5.5-.4 1.2 0 1.7.5.5 1.2.5 1.7 0l34-35c1.9-2 5.2-2.1 7.2-.1 2 1.9 2 5.2.1 7.2l-24.7 25.3c-.5.5-.4 1.2 0 1.7.5.5 1.2.5 1.7 0l28.5-29.3c2-2 5.2-2 7.1-.1 2 1.9 2 5.1.1 7.1l-28.5 29.3c-.5.5-.4 1.2 0 1.7.5.5 1.2.4 1.7 0l24.7-25.3c1.9-2 5.1-2.1 7.1-.1 2 1.9 2 5.2.1 7.2l-24.7 25.3c-.5.5-.4 1.2 0 1.7.5.5 1.2.5 1.7 0l14.6-15c2-2 5.2-2 7.2-.1 2 2 2.1 5.2.1 7.2l-27.6 28.4c-11.6 11.9-30.6 12.2-42.5.6-12-11.7-12.2-30.8-.6-42.7m18.1-48.4l-.7 4.9-2.2-4.4m7.6.9l-3.7 3.4 1.2-4.8m5.5 4.7l-4.8 1.6 3.1-3.9" />
@@ -249,7 +286,7 @@ class ClapButtonContainer extends React.Component {
         </ClapButton>
 
         <ClearClaps clapped={isClapped} onClick={this.handleClearClaps}>
-         <span> Clear {userClaps} claps</span>
+          <span> Clear {userClaps} claps</span>
         </ClearClaps>
       </ClapButtonWrap>
     );
